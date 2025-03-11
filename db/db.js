5cd353fdee68d3187135032d9fa681dcfd40de32
krishdharwal <darkpirate47171@gmail.com>
2025-03-11 16:36:11 +0530

    db name bug fixed

--- db/db.js
+++ db/db.js
@@ -1,12 +1,15 @@
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
+import { Db } from "mongodb";
 dotenv.config({
    path: './.env'
 })
 const DB_NAME = "VideoTubeDB"
 const dbConnector = async () => {
     try {
-       await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
+       await mongoose.connect(process.env.MONGO_URI,{
+        dbName: DB_NAME
+       })
        console.log("db")
     } catch (error) {
         console.log("ERROR in dbConnector : " , error);
